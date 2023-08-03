a90c906436c08c9e2c42cf5084d68ad2a9c625a3
MStenicka <martinstenicka@gmail.com>
2023-08-03 21:09:28 +0200

    updated promo action calculation and test cases

--- src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
+++ src/main/java/com/phonecompany/billing/TelephoneBillCalculatorImpl.java
@@ -10,11 +10,14 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator{
 
     @Override
     public BigDecimal calculate(String phoneLog) {
+        if (phoneLog == null || phoneLog.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
 
         Map<String, Integer> callFrequency = new HashMap<>();
         Map<String, BigDecimal> callCosts = new HashMap<>();
+        List<String> phoneNumbersWithMaxFrequency = new ArrayList<>();
 
-        String mostFrequentlyCalledNumber = null;
         int maxCallFrequency = 0;
 
         try(Scanner scanner = new Scanner(phoneLog)) {
@@ -28,63 +31,70 @@ public class TelephoneBillCalculatorImpl implements TelephoneBillCalculator{
 
                 int callDuration = calculateCallDuration(callStart, callEnd);
                 BigDecimal callCost = calculateCallCost(callStart, callDuration);
-                System.out.println("Phone number: "+ phoneNumber + " Call cost: " + callCost);
+                System.out.println("Phone number: "+ phoneNumber + ", call cost (before promo action): " + callCost + ", call duration: " + callDuration + " minutes");
 
                 callFrequency.put(phoneNumber, callFrequency.getOrDefault(phoneNumber, 0) + 1);
                 callCosts.put(phoneNumber, callCosts.getOrDefault(phoneNumber, BigDecimal.ZERO).add(callCost));
 
                 if (callFrequency.get(phoneNumber) > maxCallFrequency) {
                     maxCallFrequency = callFrequency.get(phoneNumber);
-                    mostFrequentlyCalledNumber = phoneNumber;
                 }
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Most frequently called number: "+ mostFrequentlyCalledNumber + " and max call frequency: " + maxCallFrequency);
-
-        if (mostFrequentlyCalledNumber != null) {
-            callCosts.put(mostFrequentlyCalledNumber, BigDecimal.ZERO);
+        for (Map.Entry<String, Integer> entry : callFrequency.entrySet()) { // vybere čísla s nejvyšší frekvencí volání
+            if (entry.getValue() == maxCallFrequency) {
+                phoneNumbersWithMaxFrequency.add(entry.getKey());
+            }
         }
 
-        BigDecimal totalCost = callCosts.values().stream()
+        String mostFrequentlyHighestNumber = phoneNumbersWithMaxFrequency.stream() // v případě více čísel s nejvyšší frekvencí vybere to, které je největší
+                .max(Comparator.comparing(String::toString))
+                .orElse(null);
+
+        callCosts.put(mostFrequentlyHighestNumber, BigDecimal.ZERO); // vynulování nákladů, protože nejčastěji volané číslo je zdarma
+
+        BigDecimal totalCost = callCosts.values().stream() // sečtení všech nákladů
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        System.out.println("Call frequency map: "+ callFrequency);
+        System.out.println("Call costs map: " + callCosts);
+        System.out.println("Most frequently called number (highest): "+ mostFrequentlyHighestNumber + " and max call frequency: " + maxCallFrequency + "\n");
 
         return totalCost;
         }
-
 
         private int calculateCallDuration(Date callStart, Date callEnd) {
             long durationInMilliseconds = callEnd.getTime() - callStart.getTime();
             int callDurationInSeconds = (int) (durationInMilliseconds / 1000);
-            int callDurationInMinutes = (callDurationInSeconds / 60) + ((callDurationInSeconds % 60 > 0) ? 1 : 0); // každá započatá minuta se počítá za celou
-            return callDurationInMinutes;
+            return (callDurationInSeconds / 60) + ((callDurationInSeconds % 60 > 0) ? 1 : 0); // každá započatá minuta se počítá za celou
         }
 
 
-    private BigDecimal calculateCallCost(Date callStart, int callDuration) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(callStart);
+        private BigDecimal calculateCallCost(Date callStart, int callDuration) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(callStart);
 
-        BigDecimal totalCost = BigDecimal.ZERO;
-        BigDecimal peakHourRate = new BigDecimal("1.00"); // 8h - 16h (1.00 Kč za minutu)
-        BigDecimal offPeakRate = new BigDecimal("0.50");  // 16h - 8h (0.50 Kč za minutu)
-        BigDecimal reducedRate = new BigDecimal("0.20");  // 6. a další minuta hovoru (0.20 Kč za minutu)
+            BigDecimal totalCost = BigDecimal.ZERO;
+            BigDecimal peakHourRate = new BigDecimal("1.00"); // 8h - 16h (1.00 Kč za minutu)
+            BigDecimal offPeakRate = new BigDecimal("0.50");  // 16h - 8h (0.50 Kč za minutu)
+            BigDecimal reducedRate = new BigDecimal("0.20");  // 6. a další minuta hovoru (0.20 Kč za minutu)
 
-        for (int i = 0; i < callDuration; i++) {
-            int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-            BigDecimal currentRate = (hourOfDay >= 8 && hourOfDay < 16) ? peakHourRate : offPeakRate;
+            for (int i = 0; i < callDuration; i++) {
+                int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+                BigDecimal currentRate = (hourOfDay >= 8 && hourOfDay < 16) ? peakHourRate : offPeakRate;
 
-            if (i < 5) {
-                totalCost = totalCost.add(currentRate);
-            } else {
-                totalCost = totalCost.add(reducedRate);
+                if (i < 5) {
+                    totalCost = totalCost.add(currentRate);
+                } else {
+                    totalCost = totalCost.add(reducedRate);
+                }
+
+                calendar.add(Calendar.MINUTE, 1);
             }
+            return totalCost;
+    }
 
-            calendar.add(Calendar.MINUTE, 1);
-        }
-        return totalCost;
-        }
-
-        }
+}
--- src/test/java/com/phonecompany/billing/TelephoneBillCalculatorTest.java
+++ src/test/java/com/phonecompany/billing/TelephoneBillCalculatorTest.java
@@ -9,15 +9,72 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TelephoneBillCalculatorTest {
     @Test
     public void testCalculateTotalCost() {
-        String phoneLog = "420774577451,13-01-2020 18:10:15,13-01-2020 18:17:57\n"
-                + "420774577452,18-01-2020 08:59:20,18-01-2020 09:10:00\n"
-                + "420774577452,19-01-2020 07:58:20,19-01-2020 08:01:00\n"
-                + "420774577454,19-01-2020 07:59:20,19-01-2020 08:10:00";
+        String phoneLog = """
+                420774577451,13-01-2020 18:10:15,13-01-2020 18:17:57
+                420774577452,18-01-2020 08:59:20,18-01-2020 09:10:00
+                420774577452,19-01-2020 07:58:20,19-01-2020 08:01:00
+                420774577454,19-01-2020 07:15:30,19-01-2020 07:17:10
+                420774577454,19-01-2020 07:59:20,19-01-2020 08:10:00
+                """;
+
+        TelephoneBillCalculator calculator = new TelephoneBillCalculatorImpl();
+        BigDecimal totalCost = calculator.calculate(phoneLog);
+
+        BigDecimal expectedTotalCost = new BigDecimal("11.30");
+
+        assertEquals(expectedTotalCost, totalCost);
+    }
+
+    @Test
+    public void testCalculateTotalCost2() {
+        String phoneLog = """
+                420774577451,13-01-2020 18:10:15,13-01-2020 18:17:57
+                420774577452,18-01-2020 08:59:20,18-01-2020 09:10:00
+                420774577452,19-01-2020 07:58:20,19-01-2020 08:01:00
+                420774577454,19-01-2020 07:59:20,19-01-2020 08:10:00
+                """;
 
         TelephoneBillCalculator calculator = new TelephoneBillCalculatorImpl();
         BigDecimal totalCost = calculator.calculate(phoneLog);
 
         BigDecimal expectedTotalCost = new BigDecimal("8.80");
+
+        assertEquals(expectedTotalCost, totalCost);
+    }
+
+    @Test
+    public void testCalculateTotalCostWithEmptyLog() {
+        String phoneLog = "";
+
+        TelephoneBillCalculator calculator = new TelephoneBillCalculatorImpl();
+        BigDecimal totalCost = calculator.calculate(phoneLog);
+
+        BigDecimal expectedTotalCost = BigDecimal.ZERO;
+
+        assertEquals(expectedTotalCost, totalCost);
+    }
+
+    @Test
+    public void testCalculateTotalCostWithSingleCall() {
+        String phoneLog = "420774577451,13-01-2020 18:10:15,13-01-2020 18:17:57";
+
+        TelephoneBillCalculator calculator = new TelephoneBillCalculatorImpl();
+        BigDecimal totalCost = calculator.calculate(phoneLog);
+
+        BigDecimal expectedTotalCost = new BigDecimal("0");
+
+        assertEquals(expectedTotalCost, totalCost);
+    }
+
+    @Test
+    public void testCalculateTotalCostWithMultipleCallsDuringPeakHours() {
+        String phoneLog = "420774577451,13-01-2020 08:10:15,13-01-2020 08:15:57\n"
+                + "420774577452,13-01-2020 09:30:00,13-01-2020 10:10:00";
+
+        TelephoneBillCalculator calculator = new TelephoneBillCalculatorImpl();
+        BigDecimal totalCost = calculator.calculate(phoneLog);
+
+        BigDecimal expectedTotalCost = new BigDecimal("5.20");
 
         assertEquals(expectedTotalCost, totalCost);
     }
